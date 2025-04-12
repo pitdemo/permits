4ce97a9ac63712e29596fcfaf54a5b48dc5d43f9
Anantha Kumar <anantha.kumar@thesmartq.com>
2025-04-12 16:14:33 +0530

    Heasder Cache Clear

--- sree/application/views/layouts/admin_header.php
+++ sree/application/views/layouts/admin_header.php
@@ -1,6 +1,9 @@
 <!DOCTYPE html>
 <html>
     <head>
+    <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+    <meta http-equiv="Pragma" content="no-cache" />
+    <meta http-equiv="Expires" content="0" />
         <meta charset="UTF-8">
         <title>Welcome to <?php echo $this->lang->line('site_name'); ?></title>
         <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
