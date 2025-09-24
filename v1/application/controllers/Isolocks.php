9c45cb3bb10c152a95b331091392c7f3f7bf3cac
Anantha Kumar <anantha.kumar@thesmartq.com>
2025-09-24 11:22:50 +0530

    ISO Locks Changes

--- v1/application/controllers/Isolocks.php
+++ v1/application/controllers/Isolocks.php
@@ -36,6 +36,16 @@ class Isolocks extends CI_Controller {
 		else
 			$selected_eq=array();
 
+		$status = array_search('status',$segment_array);
+		
+        if($status !==FALSE && $this->uri->segment($status+1))
+        {
+            $selected_status = $this->uri->segment($status+1);
+		}
+		
+		if($selected_status=='')
+			$selected_status=STATUS_ACTIVE;
+
         $req=array('select'=>'id,equipment_number,equipment_name','table'=>EIP_CHECKLISTS,'where_condition'=>array('status'=>STATUS_ACTIVE),'column'=>'equipment_number','dir'=>'asc');
         //Getting Active Companys List
         $qry=$this->public_model->get_data($req);
@@ -43,6 +53,8 @@ class Isolocks extends CI_Controller {
         $this->data['zones']=$qry->result_array();      
 
         $this->data['selected_eq']=$selected_eq;
+
+		$this->data['status']=$selected_status;
 		
 		$this->load->view('isolocks/tag_wise',$this->data);
 	}
@@ -72,6 +84,20 @@ class Isolocks extends CI_Controller {
             	$redirect_url.='/eq/'.$eq;
             }		
 		}	
+
+		$status = array_search('status',$segment_array);
+
+ 		if($status !==FALSE && $this->uri->segment($status+1))
+        {
+            $status = $this->uri->segment($status+1);
+
+            if($status!='')
+            {
+            	$where_condition.=' AND li.status IN("'.$status.'")';
+
+            	$redirect_url.='/status/'.$status;
+            }		
+		}	
 		
 		$limit=$_REQUEST['limit'];
 		
@@ -83,7 +109,7 @@ class Isolocks extends CI_Controller {
         
 		$totalFiltered=$this->public_model->join_fetch_data_three_tables(array('select'=>'ec.id as equipment_id','table1'=>LOTOISOLATIONS.' li','table2'=>LOTOISOLATIONSLOG.' lil','table3'=>EIP_CHECKLISTS.' ec','join_type'=>'inner','join_on_tbl2'=>'li.id=lil.jobs_lotos_id','join_on_tbl3'=>'li.eip_checklists_id=ec.id','where'=>$where_condition,'num_rows'=>true,'group_by'=>'li.id'));
 
-		$records=$this->public_model->join_fetch_data_three_tables(array('select'=>'ec.id as equipment_id,ec.equipment_name,ec.equipment_number,count(lil.id) as no_of_permits,li.isolated_tagno3,li.status,li.created,li.id as loto_id','table1'=>LOTOISOLATIONS.' li','table2'=>LOTOISOLATIONSLOG.' lil','table3'=>EIP_CHECKLISTS.' ec','join_type'=>'inner','join_on_tbl2'=>'li.id=lil.jobs_lotos_id','join_on_tbl3'=>'li.eip_checklists_id=ec.id','where'=>$where_condition,'num_rows'=>false,'group_by'=>'li.id','order_by'=>$sort_by,'order'=>$order_by,'length'=>$limit,'start'=>$start))->result_array();
+		$records=$this->public_model->join_fetch_data_three_tables(array('select'=>'ec.id as equipment_id,ec.equipment_name,ec.equipment_number,count(lil.id) as no_of_permits,SUM(if(lil.status ="'.STATUS_ACTIVE.'", 1, 0)) AS active_counts,li.isolated_tagno3,li.status,li.created,li.id as loto_id','table1'=>LOTOISOLATIONS.' li','table2'=>LOTOISOLATIONSLOG.' lil','table3'=>EIP_CHECKLISTS.' ec','join_type'=>'inner','join_on_tbl2'=>'li.id=lil.jobs_lotos_id','join_on_tbl3'=>'li.eip_checklists_id=ec.id','where'=>$where_condition,'num_rows'=>false,'group_by'=>'li.id','order_by'=>$sort_by,'order'=>$order_by,'length'=>$limit,'start'=>$start))->result_array();
 
         #echo $this->db->last_query(); exit;
 
@@ -97,11 +123,23 @@ class Isolocks extends CI_Controller {
             {
                 $json[$j]['equipment_name']=$record['equipment_name'];
 				$json[$j]['equipment_number']=$record['equipment_number'];
+				$active_counts=$record['active_counts'];
 
 				$cl='green';
 
 				if(ucfirst($record['status'])==STATUS_CLOSED)
 					$cl='red';
+				$disabled='disabled';
+				$class='';
+
+				if($active_counts==0 && $record['status']==STATUS_ACTIVE){
+					$disabled='';
+					$cl='orange';
+					$class='bulk_box checkbox';
+				}
+
+				$chk_box = "<center><input type='checkbox' name='permit_no[]' class='".$class."' ".$disabled." value='".$record['loto_id']."'><center>";
+				$json[$j]['chk_box']=$chk_box;
 
 				$json[$j]['no_of_permits']='<a href="javascript:void(0);"  data-bs-toggle="modal" data-bs-target="#modal-scrollable" data-loto-id="'.$record['loto_id'].'" data-eq="'.$record['equipment_number'].'" data-job-id="1" data-id="1" class="re_energized_log" >
                     '.$record['no_of_permits'].'
@@ -125,7 +163,18 @@ class Isolocks extends CI_Controller {
 		exit;
 	}
 
-	
+	public function ajax_update_users()
+	{
+        $response='';
+		#echo '<pre>'; print_r($_POST); exit;
+        $status = $this->input->post('status');
+        
+		$this->db->where('id IN ('.implode(',',$this->input->post('id')).')');
+        $this->db->set('status',$status);
+        $this->db->update(LOTOISOLATIONS);		
+		#echo $this->db->last_query(); 
+        echo 'bulk'; exit;
+    }
 	
 	function get_segment_array($array_args)
 	{
